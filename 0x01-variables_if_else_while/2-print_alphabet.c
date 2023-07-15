7e3d8cd7fc672ee9c3d28012929dc4aa8e0c0b62
Smart_Phil <onuohaphilip333@gmail.com>
2023-07-15 03:30:15 +0000

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase adn then in uppercase
+ *
+ *Return: Always 0(success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+
+		putchar(x);
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		if (x != 'e' && x != 'q')
+			putchar(x);
+
+	putchar('\n');
+
+	return (0);
+}
