7da36a3b19f14174db3335035a4654eb21a14ba5
RichPhilip <onuohaphilip444@gmail.com>
2025-08-04 17:04:51 +0100

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,7 @@
  */
 int main(void)
 {
-	const char *msg = "and that piece of art is useful\" - Dora Korpar, "
-			"2015-10-19\n";
-	write(2, msg, 52);
-	return (1);
+const char *msg = "nd that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+write(2, msg, 52);
+return (1);
 }
